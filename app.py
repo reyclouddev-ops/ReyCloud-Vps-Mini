bb0af7b132a26955547ae1d05b218d4e379db47b
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-22 22:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-from flask import Flask
-
-from config import Config
-
-app = Flask(__name__)
-
-app.config["SECRET_KEY"] = Config.SECRET_KEY
-
-Config.SERVERS_DIR.mkdir(exist_ok=True)
+from core.database import (
+    load_data,
+    save_data
+)
